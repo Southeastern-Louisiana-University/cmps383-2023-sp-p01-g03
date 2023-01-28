3a898b46f05c3958db6ced68ded472d1639c7725
Pupp3ttt <zakkaryhuckaby@gmail.com>
2023-01-28 16:25:35 -0600

    Comments

--- SP23.P01.Web/Controllers/TrainStationController.cs
+++ SP23.P01.Web/Controllers/TrainStationController.cs
@@ -24,12 +24,12 @@ public class TrainStationController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<TrainStationDto[]>> GetAllTrainStations()
     {
-        var result = _dataContext.TrainStations.Select(x => new TrainStationDto
+        var result = await _dataContext.TrainStations.Select(x => new TrainStationDto
         {
             Id = x.Id,
             Name = x.Name,
             Address = x.Address,
-        }).ToArray();
+        }).ToArrayAsync();
         return Ok(result);
     }
 
@@ -62,46 +62,46 @@ public class TrainStationController : ControllerBase
             return BadRequest();
         }
 
-        if (trainStationCreateDto.Name == null || trainStationCreateDto.Name == "")
+        if (trainStationCreateDto.Name == null || trainStationCreateDto.Name == "")    // If the name for the new station is empty throw an error
         {
             return BadRequest();
         }
 
-        if (trainStationCreateDto.Name.Length > 120)
+        if (trainStationCreateDto.Name.Length > 120)    // If the Name of the new station is longer than 120 characters then throw an error
         {
             return BadRequest();
         }
 
-        if (trainStationCreateDto.Address == null || trainStationCreateDto.Address == "")
+        if (trainStationCreateDto.Address == null || trainStationCreateDto.Address == "") // If the new station's address is empty then throw an error
         {
             return BadRequest();
         }
 
-        var stationNameExists = _dataContext  // Checks for if the attribute actually exists
+        var stationNameExists = _dataContext  // Checks for if the name actually exists and is in use
             .TrainStations
             .Any(x => x.Name == trainStationCreateDto.Name.Trim());
 
-        var stationAddressExists = _dataContext
+        var stationAddressExists = _dataContext     // Checks for if the address actually exists and is in use
             .TrainStations
             .Any(x => x.Address == trainStationCreateDto.Address.Trim());
 
-        if (stationAddressExists)
+        if (stationAddressExists)    // If address is in use then throw an error
         {
             return BadRequest();
         }
 
-        if (stationNameExists)
+        if (stationNameExists)   // If in name is in use then throw an error
         {
             return BadRequest();
         }
 
-        var trainStationToCreate = new TrainStation // The data actually being added
+        var trainStationToCreate = new TrainStation    // The data actually being added
         {
-            Name = trainStationCreateDto.Name.Trim(),
+            Name = trainStationCreateDto.Name.Trim(),        // Trim removes dead space in the text
             Address = trainStationCreateDto.Address.Trim()
         };
 
-        var trainStationReturn = new TrainStationDto    // The data to return to the URL
+        var trainStationReturn = new TrainStationDto     // The data to return to the URL
         {
             Id = trainStationToCreate.Id,
             Name = trainStationToCreate.Name.Trim(),
@@ -112,13 +112,15 @@ public class TrainStationController : ControllerBase
         var trainStationToReturn = _dataContext.TrainStations.Add(trainStationToCreate);
         _dataContext.SaveChanges();
 
-        var location = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}{this.Request.Path.Value}/{trainStationToReturn.Entity.Id}";
+        var location = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}{this.Request.Path.Value}/{trainStationToReturn.Entity.Id}"; // Location to send information to
 
-        return Created(location, new TrainStationDto()
+        return Created(location, new TrainStationDto() // Return the created Train Station
         {
             Id = trainStationToReturn.Entity.Id,
             Address = trainStationToReturn.Entity.Address.Trim(),
             Name = trainStationToReturn.Entity.Name.Trim()
         });
     }
+
+
 }
